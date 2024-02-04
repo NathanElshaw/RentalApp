8f9fd653eca6f3785be3e2b78157a3fdafd8a4cb
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-03 21:19:02 -0500

    Added: styling section:

---
+++ styling/MainStyles.ts
@@ -0,0 +1,17 @@
+interface styleArray {
+  [key: string]: string;
+}
+
+const stylesUtil: styleArray = {
+  mainWhite: "#FDFDFD",
+
+  mainColor: "#D90429",
+
+  accentMain: "#EF233C",
+
+  blue: "#1E1E1E",
+
+  accentBlue: "#8D99AE",
+};
+
+export default stylesUtil;
