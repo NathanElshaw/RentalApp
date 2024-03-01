81ef0b518e85e97ed3cd6c11378d5f04f44e186b
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-29 22:36:53 -0500

    Fixed: next nav container

--- Stack.tsx
+++ Stack.tsx
@@ -1,19 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ForgotPassword from "./components/forgot-password-page/ForgotPassword";
+import LoginPage from "./components/login-page/Login";
 
 const Stack = createNativeStackNavigator()
 
 const MyStack = () => {
     return (
-        <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen
+                name="Login"
+                component={LoginPage}
+                />
                 <Stack.Screen
                 name="forgotPassword"
                 component={ForgotPassword}
                 />
             </Stack.Navigator>
-        </NavigationContainer>
     )
 }
 
