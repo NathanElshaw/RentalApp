c3e042140c7b37aa47f9627bfd051bcfcf150573
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-03-03 17:41:15 -0500

    Added: create account route to app

--- App.tsx
+++ App.tsx
@@ -1,28 +1,27 @@
-import { StyleSheet} from 'react-native';
-import stylesUtil from './styling/MainStyles';
-import LoginPage from './components/login-page/Login';
-import {NavigationContainer} from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from "react-native";
+import stylesUtil from "./styling/MainStyles";
+import LoginPage from "./components/login-page/Login";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ForgotPassword from "./components/forgot-password-page/ForgotPassword";
+import Home from "./components/home-page/Home-Page";
+import CreateAccount from "./components/create-account-page/CreateAccount";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-                <Stack.Screen
-                name="Login"
-                component={LoginPage}
-                />
-                <Stack.Screen
-                name="forgotPassword"
-                component={ForgotPassword}
-                />
-                <Stack.Screen
-                name="Home"
-                component={Home}
-                />
-            </Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="forgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="createAccount" component={CreateAccount} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
@@ -31,7 +30,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: stylesUtil.mainWhite,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
