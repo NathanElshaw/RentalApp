dfe675305fdf5c8dddfbbec18b707261f3a09a92
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-03-03 17:42:43 -0500

    Added: functionaility and nav tools for new users

--- components/login-page/Login.tsx
+++ components/login-page/Login.tsx
@@ -1,93 +1,150 @@
-import { Text, StyleSheet, View, TextInput, Button } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Pressable,
+} from "react-native";
 import stylesUtil from "../../styling/MainStyles";
-import { useForm, Controller} from "react-hook-form"
+import { useForm, Controller } from "react-hook-form";
+import { ParamListBase, StackNavigationState } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-interface LoginData{
-    username: String,
-    password: String
+interface LoginData {
+  username: String;
+  password: String;
 }
 
-function LoginPage({ navigation } : any){
+function LoginPage({ navigation }: any) {
+  const forgotPasswordPress = () => {
+    navigation.navigate("forgotPassword");
+  };
 
-const {
-  control,
-  handleSubmit,
-  formState:{errors}
-} = useForm({
+  const createAccountPress = () => {
+    navigation.navigate("createAccount");
+  };
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
-        username: "",
-        password: ""
-    }
-});
+      username: "",
+      password: "",
+    },
+  });
 
-const onSubmit = (data: LoginData) => {
-    console.log(data)
-}
+  const onSubmit = (data: LoginData) => {
+    console.log(data);
+  };
 
-return  (
-<View style={loginStyles.container}>
-    <Text>JNE</Text>
-    <View style={loginStyles.loginContainer}>
-   <Controller
-   control={control}
-   rules={{
-    required: true
-   }}
-   render={({ field: {onChange, onBlur, value} }) => (
-    <TextInput
-    onBlur={onBlur}
-    onChangeText={onChange}
-    value={value}
-    style={loginStyles.inputBoxTop}
-    placeholder="Username"
-    />
-   )}
-   name="username"
-  />
-  <View
-  style={loginStyles.inputBoxBottom}>
-  <Controller
-   control={control}
-   rules={{
-    required: true
-   }}
-   render={({ field: {onChange, onBlur, value} }) => (
-    <TextInput
-    onBlur={onBlur}
-    onChangeText={onChange}
-    value={value}
-    placeholder="Password"
-    secureTextEntry
-    />
-   )}
-   name="password"
-  />
-  {errors.password && <Text>This is required</Text>}
-  </View>
-  </View>
-  <View>
-   <Button 
-   title="Submit"
-   onPress={handleSubmit(onSubmit)}
-   />
-  </View>
-  <Button title="forgotPassword" onPress={()=>{
-    navigation.navigate("forgotPassword")
-  }}/>
-</View>
-    )
+  return (
+    <View style={loginStyles.container}>
+      <Text>JNE</Text>
+      <View style={loginStyles.loginContainer}>
+        <View style={loginStyles.inputBoxTop}>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                onBlur={onBlur}
+                onChangeText={onChange}
+                style={{
+                  paddingTop: 2,
+                  paddingBottom: 2,
+                  fontSize: 18,
+                }}
+                value={value}
+                placeholder="Username"
+              />
+            )}
+            name="username"
+          />
+        </View>
+        <View style={loginStyles.inputBoxBottom}>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                onBlur={onBlur}
+                onChangeText={onChange}
+                style={{
+                  paddingTop: 2,
+                  paddingBottom: 2,
+                  fontSize: 18,
+                  width: 250,
+                }}
+                value={value}
+                placeholder="Password"
+                secureTextEntry
+              />
+            )}
+            name="password"
+          />
+          {errors.password == null ? (
+            ""
+          ) : (
+            <View style={loginStyles.inputBoxErrorContainer}>
+              <Text
+                style={{
+                  fontSize: 12,
+                  margin: 0,
+                  padding: 0,
+                  color: "red",
+                }}
+              >
+                {errors.password && (
+                  <Text
+                    style={{
+                      flex: 1,
+                      fontSize: 12,
+                      margin: 0,
+                      padding: 0,
+                    }}
+                  >
+                    This is required
+                  </Text>
+                )}
+              </Text>
+            </View>
+          )}
+        </View>
+      </View>
+      <View style={loginStyles.ctaContainer}>
+        <Pressable onPress={forgotPasswordPress}>
+          <Text>Forgot password</Text>
+        </Pressable>
+
+        <Pressable onPress={createAccountPress}>
+          <Text>Create Account</Text>
+        </Pressable>
+      </View>
+
+      <View>
+        <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      </View>
+    </View>
+  );
 }
 
 const loginStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: stylesUtil.mainWhite,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  loginContainer : {
+  loginContainer: {
     height: 90,
-    width: 260
+    width: 260,
   },
   inputBoxTop: {
     flex: 1,
@@ -95,16 +152,29 @@ const loginStyles = StyleSheet.create({
     borderTopRightRadius: 10,
     borderTopLeftRadius: 10,
     borderColor: stylesUtil.mainColor,
-    padding: 4
+    padding: 4,
   },
   inputBoxBottom: {
     flex: 1,
+    flexWrap: "wrap",
     borderWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
     borderColor: stylesUtil.mainColor,
     padding: 4,
-  }
+  },
+  inputBoxErrorContainer: {
+    margin: 0,
+    padding: 0,
+  },
+  ctaContainer: {
+    columnGap: 20,
+    marginTop: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
 });
 
 export default LoginPage;
