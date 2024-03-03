c4e948a3aaab8f07020584a636e19969356f5a7a
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-03-03 17:43:04 -0500

    Added: go back button for forgot password

--- components/forgot-password-page/ForgotPassword.tsx
+++ components/forgot-password-page/ForgotPassword.tsx
@@ -1,16 +1,16 @@
 import { Button, View } from "react-native";
 
-function ForgotPassword({navigation}: any){
-
-    return(
-        <View>
-            <Button
-            title="Back"
-            onPress={()=>{
-                navigation.navigate("")
-            }}/>
-        </View>
-    )
+function ForgotPassword({ navigation }: any) {
+  return (
+    <View>
+      <Button
+        title="Back"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
 }
 
 export default ForgotPassword;
