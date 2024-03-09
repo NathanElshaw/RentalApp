77507371f576b2e626f3f64fd1cfc16f1564a716
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-03-08 23:10:55 -0500

    Added: conditional rendering for form steps and starts of types

--- components/create-account-page/CreateAccount.tsx
+++ components/create-account-page/CreateAccount.tsx
@@ -1,10 +1,23 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import stylesUtil from "../../styling/MainStyles";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface signUpForm {}
 
 function CreateAccount({ navigation }: any) {
   const [formType, setFormType] = useState<String>("Default");
   const [formStep, setFormStep] = useState<number>(0);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const apiCreateAccount = (data: signUpForm) => {
+    formType == "Default" ? "Api req no code" : "Api req with code";
+  };
 
   const proceedCreateAccount = () => {
     setFormType("Default");
@@ -14,6 +27,10 @@ function CreateAccount({ navigation }: any) {
   const proceedWithCode = () => {
     setFormType("With Code");
     setFormStep(formStep + 1);
+  };
+
+  const goBack = () => {
+    formStep > 0 ? setFormStep(formStep - 1) : null;
   };
 
   return (
@@ -29,24 +46,34 @@ function CreateAccount({ navigation }: any) {
             navigation.goBack();
           }}
         >
+          <Text>{"<"}</Text>
+        </Pressable>
+      </View>
+      {formStep == 0 ? (
+        <View>
+          <Text>Start creating an account!</Text>
+          <View style={SignUpStyles.ctaContainer}>
+            <Pressable
+              style={SignUpStyles.createAccountButton}
+              onPress={proceedCreateAccount}
+            >
+              <Text>Create Account</Text>
+            </Pressable>
+            <Pressable
+              style={SignUpStyles.codeAccountButton}
+              onPress={proceedWithCode}
+            >
+              <Text>I have a code!</Text>
+            </Pressable>
+          </View>
+        </View>
+      ) : formStep == 1 ? (
+        <Pressable onPress={goBack}>
           <Text>Back</Text>
         </Pressable>
-      </View>
-      <Text>Start creating an account!</Text>
-      <View style={SignUpStyles.ctaContainer}>
-        <Pressable
-          style={SignUpStyles.createAccountButton}
-          onPress={proceedCreateAccount}
-        >
-          <Text>Create Account</Text>
-        </Pressable>
-        <Pressable
-          style={SignUpStyles.codeAccountButton}
-          onPress={proceedWithCode}
-        >
-          <Text>I have a code!</Text>
-        </Pressable>
-      </View>
+      ) : (
+        ""
+      )}
     </View>
   );
 }
