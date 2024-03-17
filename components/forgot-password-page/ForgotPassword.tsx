39f55b8be740609bf8f3c44a4f66c0bbe651e17d
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-03-17 17:35:25 -0400

    Added: form area for requesting new password

--- components/forgot-password-page/ForgotPassword.tsx
+++ components/forgot-password-page/ForgotPassword.tsx
@@ -1,16 +1,153 @@
-import { Button, View } from "react-native";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import stylesUtil from "../../styling/MainStyles";
+import { Controller, useForm } from "react-hook-form";
 
 function ForgotPassword({ navigation }: any) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const createAccountPress = () => {
+    navigation.navigate("createAccount");
+  };
+
   return (
-    <View>
-      <Button
-        title="Back"
-        onPress={() => {
-          navigation.goBack();
-        }}
-      />
+    <View style={fpStyle.container}>
+      <View style={fpStyle.formContainer}>
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 24,
+            fontWeight: "600",
+            marginVertical: 4,
+          }}
+        >
+          Forgot Password
+        </Text>
+        <Text
+          style={{
+            textAlign: "center",
+            width: 275,
+            marginBottom: 10,
+          }}
+        >
+          Enter the email address associated with your account and we'll send
+          you a link to reset your password
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: "400",
+            marginBottom: 4,
+          }}
+        >
+          Email:
+        </Text>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              onBlur={onBlur}
+              onChangeText={onChange}
+              style={fpStyle.formInput}
+              value={value}
+              placeholder="Email@example.com"
+            />
+          )}
+          name="email"
+        />
+        <Text
+          style={{
+            fontSize: 12,
+          }}
+        ></Text>
+        <Pressable style={fpStyle.ctaButton}>
+          <Text
+            style={{
+              color: stylesUtil.mainWhite,
+              fontSize: 16,
+              fontWeight: "600",
+            }}
+          >
+            Reset password
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={fpStyle.createAccCta}>
+        <Text
+          style={{
+            fontSize: 16,
+          }}
+        >
+          Don't have an Account?
+        </Text>
+        <Pressable onPress={createAccountPress}>
+          <Text
+            style={{
+              color: "blue",
+              fontSize: 16,
+            }}
+          >
+            Register
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const fpStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderWidth: 1,
+    backgroundColor: stylesUtil.mainWhite,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  formContainer: {
+    flex: 10,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  formInput: {
+    borderWidth: 1,
+    borderBottomColor: "black",
+    borderColor: stylesUtil.mainWhite,
+    marginHorizontal: 10,
+    width: 250,
+    borderRadius: 5,
+    marginBottom: 5,
+    paddingHorizontal: 10,
+    paddingBottom: 4,
+    fontSize: 18,
+  },
+  ctaButton: {
+    backgroundColor: stylesUtil.mainColor,
+    width: 260,
+    paddingVertical: 8,
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  createAccCta: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    columnGap: 4,
+    paddingBottom: 40,
+  },
+});
 
 export default ForgotPassword;
