e5aeb5afd8472ebd635418c8382c40aa5206b130
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-03-20 23:11:44 -0400

    Added: start of header

--- components/home-page/Home-Page.tsx
+++ components/home-page/Home-Page.tsx
@@ -1,11 +1,69 @@
-import { View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import stylesUtil from "../../styling/MainStyles";
 
-function Home(){
-    return(
-        <View>
-            Home
+const screenWidth = Dimensions.get("window").width;
+
+const screenHeight = Dimensions.get("window").height;
+
+function Home({ navigation }: any) {
+  return (
+    <View style={homeStyles.container}>
+      <View style={homeStyles.headerContainer}>
+        <View
+          style={{
+            flex: 10,
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            borderWidth: 1,
+          }}
+        >
+          <Image
+            style={{ width: 50, height: 50 }}
+            source={require("../../assets/iconMain.png")}
+          />
         </View>
-    )
-};
+        <View
+          style={{
+            flex: 1,
+            flexWrap: "wrap",
+          }}
+        >
+          <Pressable>
+            <Text>Account</Text>
+          </Pressable>
+        </View>
+      </View>
+      <View style={homeStyles.mainContainer}></View>
+    </View>
+  );
+}
+
+const homeStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: stylesUtil.mainWhite,
+  },
+  headerContainer: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "black",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    marginTop: screenHeight * 0.05,
+    marginHorizontal: screenWidth * 0.02,
+  },
+  mainContainer: {
+    flex: 10,
+  },
+});
 
 export default Home;
