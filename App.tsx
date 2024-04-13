ad9b5ccfcb8caa585eec40890b9d206c81a54c8a
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-04-13 14:58:47 -0400

    Added: components to stack

--- App.tsx
+++ App.tsx
@@ -1,10 +1,7 @@
 import { StyleSheet } from "react-native";
 import stylesUtil from "./styling/MainStyles";
 import LoginPage from "./components/login-page/Login";
-import {
-  NavigationContainer,
-  getFocusedRouteNameFromRoute,
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ForgotPassword from "./components/forgot-password-page/ForgotPassword";
 import Home from "./components/home-page/Home-Page";
@@ -12,6 +9,8 @@ import CreateAccount from "./components/create-account-page/CreateAccount";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import PaymentPage from "./components/charges-tab/PaymentPage";
+import Requests from "./components/request-tab/Requests";
+import Settings from "./components/settings-tab/Settings";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -29,8 +28,10 @@ export default function App() {
         <Stack.Screen name="forgotPassword" component={ForgotPassword} />
         <Stack.Screen name="createAccount" component={CreateAccount} />
 
-        <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="MakePayment" component={PaymentPage} />
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Requests" component={Requests} />
+        <Tab.Screen name="SettingsPage" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
   );
