f3f98927381fc645c992f26905a249599879860e
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-04-16 21:59:15 -0400

    Added: more work on settings config

--- components/settings-tab/Settings.tsx
+++ components/settings-tab/Settings.tsx
@@ -4,6 +4,7 @@ import Footer from "../globals/Footer";
 import Header from "../globals/Header";
 import { useEffect, useState } from "react";
 import * as SecureStore from "expo-secure-store";
+import { settingsType } from "./settingsConfig";
 interface settingProps {
   navigation: any;
 }
@@ -18,20 +19,22 @@ const screenHeight: number = Dimensions.get("screen").height;
 const Settings: React.FC<settingProps> = (props: settingProps) => {
   const navigation = props.navigation;
 
-  const [settingConfig, setSettingConfig] = useState<string>();
-
-  const getSettings = async () => {
-    let settings: string | null = await SecureStore.getItemAsync(
-      "defaultSettings"
-    );
-    if (settings) {
-      setSettingConfig(settings);
-    }
-  };
+  const [settingConfig, setSettingConfig] = useState<settingsType>();
+  useEffect(() => {
+    const getSettings = async () => {
+      let settings: string | null = await SecureStore.getItemAsync(
+        "defaultSettings"
+      );
+      if (settings && settingConfig == null) {
+        const parsedSettings = await JSON.parse(settings);
+        setSettingConfig(parsedSettings);
+      }
+    };
+    getSettings();
+  }, [settingConfig]);
 
   const [darkMode, setDarkMode] = useState<boolean>(false);
 
-  console.log(settingConfig);
   return (
     <View style={settingsPage.container}>
       <Header navigation={navigation} />
@@ -42,65 +45,79 @@ const Settings: React.FC<settingProps> = (props: settingProps) => {
             marginTop: screenHeight * 0.025,
           }}
         >
-          <View
-            style={{
-              borderWidth: 1,
-              borderRadius: 10,
-              width: screenWidth * 0.85,
-              height: screenHeight * 0.075,
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
-            <View
-              style={{
-                alignSelf: "center",
-                marginHorizontal: screenWidth * 0.05,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontWeight: "500",
-                }}
-              >
-                Setting id
-              </Text>
-            </View>
-            <View
-              style={{
-                alignSelf: "center",
-                marginHorizontal: screenWidth * 0.05,
-              }}
-            >
-              <Pressable
-                onPress={() => {
-                  setDarkMode(!darkMode);
-                }}
-              >
-                <View
-                  style={{
-                    borderWidth: 1,
-                    borderRadius: 25,
-                    width: screenWidth * 0.05,
-                    height: screenWidth * 0.05,
-                    borderColor:
-                      darkMode == true ? stylesUtil.mainColor : "black",
-                    backgroundColor:
-                      darkMode == true
-                        ? stylesUtil.mainColor
-                        : stylesUtil.mainWhite,
-                  }}
-                ></View>
-              </Pressable>
-            </View>
-          </View>
+          {settingConfig != null
+            ? Object.keys(settingConfig).map((id: string, value: number) => {
+                return (
+                  <View
+                    key={id}
+                    style={{
+                      borderWidth: 1,
+                      borderRadius: 10,
+                      width: screenWidth * 0.85,
+                      height: screenHeight * 0.075,
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      marginVertical: screenHeight * 0.01,
+                    }}
+                  >
+                    <View
+                      style={{
+                        alignSelf: "center",
+                        marginHorizontal: screenWidth * 0.05,
+                      }}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          fontWeight: "500",
+                        }}
+                      >
+                        {id}
+                      </Text>
+                    </View>
+                    <View
+                      style={{
+                        alignSelf: "center",
+                        marginHorizontal: screenWidth * 0.05,
+                      }}
+                    >
+                      {value == typeof Boolean ? (
+                        <Pressable
+                          onPress={() => {
+                            setDarkMode(!darkMode);
+                          }}
+                        >
+                          <View
+                            style={{
+                              borderWidth: 1,
+                              borderRadius: 25,
+                              width: screenWidth * 0.05,
+                              height: screenWidth * 0.05,
+                              borderColor:
+                                darkMode == true
+                                  ? stylesUtil.mainColor
+                                  : "black",
+                              backgroundColor:
+                                darkMode == true
+                                  ? stylesUtil.mainColor
+                                  : stylesUtil.mainWhite,
+                            }}
+                          ></View>
+                        </Pressable>
+                      ) : (
+                        <Text>{value}</Text>
+                      )}
+                    </View>
+                  </View>
+                );
+              })
+            : ""}
           <Text
             style={{
               color: "grey",
             }}
           >
-            Version: {"1.0.0"}
+            Version: {settingConfig != null ? settingConfig.version : "1.1.0"}
           </Text>
         </View>
       </View>
--- components/settings-tab/settingsConfig.ts
+++ components/settings-tab/settingsConfig.ts
@@ -1,1 +1,13 @@
-import * as SecureStore from "expo-secure-store";
+export interface settingsType {
+  darkMode: boolean;
+  testSetting: string;
+  version: string;
+}
+
+const defaultSettings: settingsType = {
+  darkMode: true,
+  testSetting: "test",
+  version: "1.0.0",
+};
+
+export const defaultSettingsStore = JSON.stringify(defaultSettings);
