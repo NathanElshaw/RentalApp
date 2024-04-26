9c63efb9fb36c392d7aacce6b613d949964975c3
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-04-25 23:46:42 -0400

    Removed: no unit function

--- components/home-page/Home-Page.tsx
+++ components/home-page/Home-Page.tsx
@@ -1,15 +1,5 @@
-import {
-  Dimensions,
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
 import stylesUtil from "../../styling/MainStyles";
-import { Controller, useForm } from "react-hook-form";
-import Spinner from "../globals/Spinner";
-import { useState } from "react";
 import Footer from "../globals/Footer";
 import Header from "../globals/Header";
 import { screenHeight, screenWidth } from "../globals/Defaults";
@@ -59,8 +49,6 @@ const dummyUser: user = {
 };
 
 function Home({ navigation }: any) {
-  const [menuItem, setMenuItem] = useState<number>(3);
-
   return (
     <View style={homeStyles.container}>
       <Header navigation={navigation} />
@@ -72,123 +60,6 @@ function Home({ navigation }: any) {
     </View>
   );
 }
-
-const NoUnitCta = () => {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors, isSubmitting, isSubmitted },
-  } = useForm({
-    defaultValues: {
-      unitCode: "",
-    },
-  });
-
-  const clearError = () => {
-    if (errors.unitCode) errors.unitCode.type = "";
-  };
-
-  const onSubmit = (data: any) => {
-    console.log(isSubmitting);
-  };
-
-  return (
-    <View style={homeStyles.joinUnitCta}>
-      {isSubmitted == false ? (
-        <View>
-          <Text
-            style={{
-              textAlign: "center",
-              width: screenWidth - 100,
-              fontSize: 26,
-              fontWeight: "700",
-              marginBottom: screenHeight * 0.01,
-            }}
-          >
-            Join a Unit!
-          </Text>
-          <Text
-            style={{
-              textAlign: "center",
-              width: screenWidth - 100,
-              fontSize: 14,
-              fontWeight: "400",
-              marginBottom: screenHeight * 0.01,
-            }}
-          >
-            You currently arent apart of a unit, enter your unit code to join
-            your unit
-          </Text>
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "600",
-              marginBottom: screenHeight * 0.01,
-            }}
-          >
-            Code:
-          </Text>
-          <View style={homeStyles.formContainer}>
-            <Controller
-              control={control}
-              rules={{
-                required: true,
-              }}
-              render={({ field: { onChange, onBlur, value } }) => (
-                <TextInput
-                  onBlur={onBlur}
-                  onChangeText={onChange}
-                  onFocus={clearError}
-                  style={{
-                    ...homeStyles.formInput,
-                    borderColor:
-                      errors.unitCode?.type === "required"
-                        ? "red"
-                        : stylesUtil.mainWhite,
-                    borderBottomColor:
-                      errors.unitCode?.type === "required" ? "red" : "black",
-                  }}
-                  value={value}
-                  placeholder="Join code"
-                />
-              )}
-              name={"unitCode"}
-            />
-            <View style={homeStyles.formErrorContainer}>
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: "500",
-                  color: "red",
-                }}
-              >
-                {errors.unitCode?.type === "required"
-                  ? "This field is required"
-                  : ""}
-              </Text>
-            </View>
-            <Pressable
-              style={homeStyles.joinUnitButton}
-              onPress={handleSubmit(onSubmit)}
-            >
-              <Text
-                style={{
-                  color: stylesUtil.mainWhite,
-                  fontSize: 18,
-                  fontWeight: "600",
-                }}
-              >
-                Join
-              </Text>
-            </Pressable>
-          </View>
-        </View>
-      ) : (
-        <Spinner color={stylesUtil.mainColor} />
-      )}
-    </View>
-  );
-};
 
 const homeStyles = StyleSheet.create({
   container: {
