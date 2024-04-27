76f5a69797b6a8ebf1d4817215a5ca55c615c83d
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-04-26 23:26:58 -0400

    Added: import type

--- components/home-page/Admin-Page.tsx
+++ components/home-page/Admin-Page.tsx
@@ -1,28 +1,5 @@
 import { View } from "react-native";
-
-interface HomePageProps {
-  user: user;
-  navigation: any;
-}
-
-interface issuesType {
-  name: string;
-  issue: string;
-  date: string;
-  state: string;
-}
-
-interface user {
-  name: string;
-  username: string;
-  housingInfo: {
-    unitNumber?: number;
-    address: string;
-    rent: number;
-    paid: number;
-  };
-  issues?: issuesType[];
-}
+import { HomePageProps } from "./Home-Page";
 
 const AdminHomePage: React.FC<HomePageProps> = (props: HomePageProps) => {
   return <View></View>;
