696556a5d08a16b3f0df8910c1c21bcb6b29c294
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-04-30 22:51:38 -0400

    Added: export of rental type

--- components/rental-info-tab/Rental.tsx
+++ components/rental-info-tab/Rental.tsx
@@ -23,24 +23,13 @@ interface rentalData {
   leaseEnd: string;
 }
 
-const dummyRental: rentalData = {
-  unitAddress: "123 testing avg",
-  beds: 2,
-  baths: 1.5,
-  unitNumber: 12,
-  hasPets: true,
-  renterAmount: 2,
-  rentAmount: 1400,
-  rentDue: 0,
-  rentPaid: 1400,
-  leaseStart: "2024-1-1",
-  rentDueDate: 4,
-  leaseEnd: "2025-1-1",
-};
-
 const Rental: React.FC<rentalProps> = (props: rentalProps) => {
   const navigation = props.navigation;
   const rental = props.rental;
+
+  if (!rental) {
+    //fetch new rental info
+  }
 
   return (
     <View style={rentalPage.container}>
@@ -65,3 +54,5 @@ const rentalPage = StyleSheet.create({
     flex: 12,
   },
 });
+
+export { rentalData };
