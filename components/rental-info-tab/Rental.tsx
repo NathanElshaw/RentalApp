230d32ccf9ac319b21bf244a46feffe19d8b360c
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-05-01 23:06:05 -0400

    Added: start of displaying rental data

--- components/rental-info-tab/Rental.tsx
+++ components/rental-info-tab/Rental.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import stylesUtil from "../../styling/MainStyles";
 import Footer from "../globals/Footer";
 import Header from "../globals/Header";
@@ -23,9 +23,24 @@ interface rentalData {
   leaseEnd: string;
 }
 
+const dummyRental: rentalData = {
+  unitAddress: "123 test ave",
+  beds: 1,
+  baths: 1.5,
+  unitNumber: 12,
+  hasPets: false,
+  renterAmount: 2,
+  rentAmount: 1400,
+  rentDue: 0,
+  rentPaid: 1400,
+  leaseStart: "2024-1-1",
+  rentDueDate: 1,
+  leaseEnd: "2025-1-1",
+};
+
 const Rental: React.FC<rentalProps> = (props: rentalProps) => {
   const navigation = props.navigation;
-  const rental = props.rental;
+  const rental = dummyRental;
 
   if (!rental) {
     //fetch new rental info
@@ -35,7 +50,29 @@ const Rental: React.FC<rentalProps> = (props: rentalProps) => {
     <View style={rentalPage.container}>
       <Header navigation={navigation} />
       <View style={rentalPage.mainContainer}>
-        <Text>Rental</Text>
+        <View>
+          <Text>Address: {rental?.unitAddress}</Text>
+          <Text>
+            {!rental.unitNumber ? "" : `unit number: ${rental.unitNumber}`}
+          </Text>
+        </View>
+        <View>
+          <Text>Beds: {rental.beds}</Text>
+          <Text>Baths: {rental.baths}</Text>
+        </View>
+        <View>
+          <View>
+            <Text>Rent amount: ${rental.rentAmount}</Text>
+            <Text>Amount owed: ${rental.rentDue}</Text>
+          </View>
+          <View>
+            <Pressable>
+              <Text>
+                {rental.rentDue === 0 ? "All caught up!" : "Make Payment"}
+              </Text>
+            </Pressable>
+          </View>
+        </View>
       </View>
       <Footer defaultIcon={1} navigation={navigation} />
     </View>
